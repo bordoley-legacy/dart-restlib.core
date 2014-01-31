f47b799f109fff1f4316a3a7738a0e05e496d59d
David Bordoley <bordoley@gmail.com>
2014-01-31 00:34:05 -0800

    remove unused test.

--- test/src/http_syntax/matchers_test.dart
+++ test/src/http_syntax/matchers_test.dart
@@ -1,18 +1,6 @@
 part of restlib.http_syntax_test;
 
-matchersTests() {
-  test("WS", (){
-    for (int i = 0; i < 0x100; i++) {
-      bool match = WS.matches(i);
-      if (i != "\t".runes.single && 
-          i != " ".runes.single) {
-        expect(match, isFalse);
-      } else {
-        expect(match, isTrue);
-      }
-    }
-  });
-  
+matchersTests() {  
   test("OBS_TEXT", (){
     for (int i = 0; i < 0x100; i++) {
       bool match = OBS_TEXT.matches(i);
