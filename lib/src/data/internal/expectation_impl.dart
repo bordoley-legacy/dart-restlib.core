d5794f4013e1a75574ac29e1aba6edf916b91239
David Bordoley <bordoley@gmail.com>
2014-02-19 00:36:41 -0800

    remove const on constructor for now.

--- lib/src/data/internal/expectation_impl.dart
+++ lib/src/data/internal/expectation_impl.dart
@@ -5,47 +5,47 @@ final Parser<Expectation> EXPECTATION =
     .map((final Iterable<KeyValuePair> kvp) =>
         new ExpectationImpl(EMPTY_SEQUENCE_MULTIMAP.putAll(kvp)));
 
-class ExpectationImpl 
+class ExpectationImpl
     extends Object
     with ForwardingIterable<Pair<String,String>>,
       ForwardingAssociative<String, String>,
       ForwardingMultimap<String, String, Sequence<String>>
     implements Expectation {
   final ImmutableSequenceMultimap<String,String> delegate;
-  
-  const ExpectationImpl(this.delegate);
-  
+
+  ExpectationImpl(this.delegate);
+
   int get hashCode => delegate.hashCode;
-  
+
   bool operator==(other) =>
       delegate == other;
-  
+
   Expectation put(final String key, final String value) {
     checkArgument(TOKEN.parse(key).isNotEmpty);
     checkArgument(QUOTABLE.matchesAllOf(value));
-    
+
     return new ExpectationImpl(delegate.put(key, value));
   }
-      
+
   Expectation putAll(final Iterable<Pair<String, String>> other) =>
       other.fold(this, (final Expectation acc, final Pair<String, String> pair) =>
           acc.putPair(pair));
-  
+
   Expectation putAllFromMap(Map<String, String> map) {
     Expectation retval = this;
     map.forEach((final String key, final String value) {
       retval = retval.put(key, value);
     });
-    
+
     return retval;
   }
-  
+
   Expectation putPair(Pair<String, String> pair) =>
       put(pair.fst, pair.snd);
-  
+
   Expectation removeAt(final String key) =>
       new ExpectationImpl(delegate.removeAt(key));
-  
+
   String toString() =>
       pairIterableToString(this);
 }
