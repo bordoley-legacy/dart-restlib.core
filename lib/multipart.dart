05cbaf1398816dccdb2f1bb385541bb0b2fa7004
David Bordoley <bordoley@gmail.com>
2014-02-24 21:25:12 -0800

    Change parseMultipartStream to return Option<Multipart> and to return Option.NONE on MimeMultipartException.

--- lib/multipart.dart
+++ lib/multipart.dart
@@ -21,9 +21,9 @@ part "src/multipart/multipart_impl.dart";
 abstract class Multipart<T> implements Iterable<Part<T>> {
   factory Multipart(final String boundary, final Iterable<Part<T>> parts) =>
       new _Multipart(boundary, parts);
-  
+
   String get boundary;
-} 
+}
 
 abstract class ByteStreamableMultipart<T extends ByteStreamable> implements Multipart<T>, ByteStreamable {
   factory ByteStreamableMultipart(final String boundary, final Iterable<Part<T>> parts) =>
@@ -36,9 +36,9 @@ class _ByteStreamableMultipart<T extends ByteStreamable>
     implements ByteStreamableMultipart<T> {
   final String boundary;
   final Iterable<Part<ByteStreamable>> delegate;
-  
+
   _ByteStreamableMultipart(this.boundary, this.delegate);
-  
+
   Stream<List<int>> asByteStream() {
     final ImmutableSequence<Stream<List<int>>> streams =
         this
@@ -50,13 +50,13 @@ class _ByteStreamableMultipart<T extends ByteStreamable>
                 .add(valueAsStream(ASCII.encode("\r\n\r\n"))))
           .add(valueAsStream(ASCII.encode("--$boundary--\r\n")));
     return concatStreams(streams);
-  }    
+  }
 }
 
 // Use this to include custom part headers
 abstract class PartContentInfo implements ContentInfo {
   Dictionary<Header, dynamic> get customHeaders;
-  
+
   PartContentInfo with_({
     Dictionary<Header, dynamic> customHeaders,
     Iterable<ContentEncoding> encodings,
@@ -64,9 +64,9 @@ abstract class PartContentInfo implements ContentInfo {
     int length,
     URI location,
     MediaRange mediaRange,
-    ContentRange range 
+    ContentRange range
   });
-  
+
   ContentInfo without({
     final bool customHeaders: false,
     final bool encodings: false,
@@ -81,7 +81,7 @@ abstract class PartContentInfo implements ContentInfo {
 class Part<T> {
   final PartContentInfo contentInfo;
   final T entity;
-  
+
   Part(final ContentInfo contentInfo, this.entity):
       this.contentInfo = new _PartContentInfo(EMPTY_DICTIONARY, contentInfo) {
     checkNotNull(contentInfo);
@@ -91,26 +91,27 @@ class Part<T> {
 
 typedef Future<Part> PartParser(PartContentInfo contentInfo, final Stream<List<int>> msgStream);
 
-Future<Multipart> parseMultipartStream(
-    final Stream<List<int>> msgStream, 
-    final String boundary, 
-    Option<PartParser> partParserProvider(PartContentInfo contentInfo)) => 
+Future<Option<Multipart>> parseMultipartStream(
+    final Stream<List<int>> msgStream,
+    final String boundary,
+    Option<PartParser> partParserProvider(PartContentInfo contentInfo)) =>
         new MimeMultipartTransformer(boundary)
           .bind(msgStream)
           .map((final MimeMultipart multipart) =>
               new Part(new ContentInfo.wrapHeaders(
-                  EMPTY_SEQUENCE_MULTIMAP.putAllFromMap(multipart.headers)), 
+                  EMPTY_SEQUENCE_MULTIMAP.putAllFromMap(multipart.headers)),
               multipart))
-          .fold(EMPTY_SEQUENCE, (final ImmutableSequence<Future> futureResults, final Part<Stream<List<int>>> part) => 
+          .fold(EMPTY_SEQUENCE, (final ImmutableSequence<Future> futureResults, final Part<Stream<List<int>>> part) =>
               partParserProvider(part.contentInfo)
                 .map((final PartParser parser) =>
                     futureResults.add(parser(part.contentInfo, part.entity)))
                 .orCompute(() =>
-                    futureResults.add(part.entity.drain()))
+                    futureResults.add(part.entity.drain())))
           .then(Future.wait)
-          .then((final List parts) => 
+          .then((final List parts) =>
               parts.where((final e) =>
                   e is Part))
           .then((final List<Part> parts) =>
-              new Multipart(boundary, parts)));   
+              new Option (new Multipart(boundary, parts)))
+          .catchError((final MimeMultipartException e) => Option.NONE, test: (final e) => e is MimeMultipartException);
 
