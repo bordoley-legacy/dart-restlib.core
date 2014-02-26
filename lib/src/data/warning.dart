1a16d5e5768e643c31b0e2f6174bbe6e7b996e91
David Bordoley <bordoley@gmail.com>
2014-02-26 09:10:13 -0800

    Add parser to warning.

--- lib/src/data/warning.dart
+++ lib/src/data/warning.dart
@@ -1,25 +1,27 @@
 part of restlib.core.data;
 
 abstract class Warning {
+  static final Parser<Warning> parser = WARNING;
+
   Either<Authority, String> get warnAgent;
   int get warnCode;
   Option<DateTime> get warnDate;
   String get warnText;
-  
+
   factory Warning.hostPort(final int warnCode, final Authority warnAgent, final String warnText, [final DateTime warnDate = null]) {
       checkArgument(warnCode > 0);
       checkArgument(warnCode <= 999);
       checkArgument(QUOTABLE.matchesAllOf(warnText));
       checkArgument(warnAgent.userInfo.isEmpty);
-      
+
       return new WarningImpl(warnCode, new Either.leftValue(warnAgent), warnText, new Option(warnDate));
     }
-    
+
   factory Warning.synomynAgent(final int warnCode, final String warnAgent, final String warnText, [final DateTime warnDate = null]) {
     checkArgument(warnCode > 0);
     checkArgument(warnCode <= 999);
     checkArgument(QUOTABLE.matchesAllOf(warnText));
-      
+
     return new WarningImpl(warnCode, new Either.rightValue(warnAgent), warnText, new Option(warnDate));
   }
 }
