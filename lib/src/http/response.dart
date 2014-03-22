da2f61cc67c636d53e2eb8b8baef1a821a05afbb
David Bordoley <bordoley@gmail.com>
2014-03-22 11:33:09 -0700

    simplify response implementation.

--- lib/src/http/response.dart
+++ lib/src/http/response.dart
@@ -26,132 +26,6 @@ void writeResponseHeaders(final Response response, void writeHeaderLine(final St
       _writeHeader(header.e0, header.e1, writeHeaderLine));
 }
 
-String _responseToString(final Response response) {
-  final StringBuffer buffer = new StringBuffer();
-
-  _WriteHeaderLine writeHeaderLine = _writeHeaderToBuffer(buffer);
-  buffer.write("${response.status}\r\n");
-
-  writeResponseHeaders(response, writeHeaderLine);
-
-  buffer.write(response.entity.map((final entity) =>
-      "\r\n\r\n${entity.toString()}\r\n").orElse(""));
-
-  return buffer.toString();
-}
-
-Response _responseWith(
-  final Response delegate,
-  final Iterable<RangeUnit> acceptedRangeUnits,
-  final int age,
-  final Iterable<Method> allowedMethods,
-  final Iterable<ChallengeMessage> authenticationChallenges,
-  final Iterable<CacheDirective> cacheDirectives,
-  final ContentInfo contentInfo,
-  final Dictionary<Header, dynamic> customHeaders,
-  final DateTime date,
-  final entity,
-  final EntityTag entityTag,
-  final DateTime expires,
-  final DateTime lastModified,
-  final URI location,
-  final Iterable<ChallengeMessage> proxyAuthenticationChallenges,
-  final DateTime retryAfter,
-  final UserAgent server,
-  final Iterable<SetCookie> setCookies,
-  final Status status,
-  final Iterable<Header> vary,
-  final Iterable<Warning> warnings) {
-
-  if(isNull(acceptedRangeUnits) &&
-      isNull(age) &&
-      isNull(allowedMethods) &&
-      isNull(authenticationChallenges) &&
-      isNull(cacheDirectives) &&
-      isNull(contentInfo) &&
-      isNull(customHeaders) &&
-      isNull(date) &&
-      isNull(entity) &&
-      isNull(entityTag) &&
-      isNull(expires) &&
-      isNull(lastModified) &&
-      isNull(location) &&
-      isNull(proxyAuthenticationChallenges) &&
-      isNull(retryAfter) &&
-      isNull(server) &&
-      isNull(setCookies) &&
-      isNull(status) &&
-      isNull(vary) &&
-      isNull(warnings)) {
-    return delegate;
-  }
-
-  return new _ResponseImpl(
-      EMPTY_SET.addAll(firstNotNull(acceptedRangeUnits, delegate.acceptedRangeUnits)),
-      computeIfEmpty(new Option(age), () => delegate.age),
-      EMPTY_SET.addAll(firstNotNull(allowedMethods, delegate.allowedMethods)),
-      EMPTY_SET.addAll(firstNotNull(authenticationChallenges, delegate.authenticationChallenges)),
-      EMPTY_SET.addAll(firstNotNull(cacheDirectives, delegate.cacheDirectives)),
-      firstNotNull(contentInfo, delegate.contentInfo),
-      EMPTY_DICTIONARY.putAll(firstNotNull(customHeaders, delegate.customHeaders)),
-      computeIfEmpty(new Option(date), () => delegate.date),
-      computeIfEmpty(new Option(entity), () => delegate.entity),
-      computeIfEmpty(new Option(entityTag), () => delegate.entityTag),
-      computeIfEmpty(new Option(expires), () => delegate.expires),
-      computeIfEmpty(new Option(lastModified), () => delegate.lastModified),
-      computeIfEmpty(new Option(location), () => delegate.location),
-      EMPTY_SET.addAll(firstNotNull(proxyAuthenticationChallenges, delegate.proxyAuthenticationChallenges)),
-      computeIfEmpty(new Option(retryAfter), () => delegate.retryAfter),
-      computeIfEmpty(new Option(server), () => delegate.server),
-      EMPTY_SET.addAll(firstNotNull(setCookies, delegate.setCookies)),
-      firstNotNull(status, delegate.status),
-      EMPTY_SET.addAll(firstNotNull(vary, delegate.vary)),
-      EMPTY_SEQUENCE.addAll(firstNotNull(warnings, delegate.warnings)));
-}
-
-Response _responseWithout(
-  final Response delegate,
-  final bool acceptedRangeUnits,
-  final bool age,
-  final bool allowedMethods,
-  final bool authenticationChallenges,
-  final bool cacheDirectives,
-  final bool contentInfo,
-  final bool customHeaders,
-  final bool date,
-  final bool entity,
-  final bool entityTag,
-  final bool expires,
-  final bool lastModified,
-  final bool location,
-  final bool proxyAuthenticationChallenges,
-  final bool retryAfter,
-  final bool server,
-  final bool setCookies,
-  final bool vary,
-  final bool warnings) =>
-      new _ResponseImpl(
-          !acceptedRangeUnits ? EMPTY_SET.addAll(delegate.acceptedRangeUnits) : EMPTY_SET,
-          !age ? delegate.age : Option.NONE,
-          !allowedMethods ? EMPTY_SET.addAll(delegate.allowedMethods) : EMPTY_SET,
-          !authenticationChallenges ? EMPTY_SET.addAll(delegate.authenticationChallenges) : EMPTY_SET,
-          !cacheDirectives ? EMPTY_SET.addAll(delegate.authenticationChallenges) : EMPTY_SET,
-          !contentInfo ? delegate.contentInfo : ContentInfo.NONE,
-          !customHeaders ? EMPTY_DICTIONARY.putAll(delegate.customHeaders) : EMPTY_DICTIONARY,
-          !date ? delegate.date : Option.NONE,
-          !entity ? delegate.entity : Option.NONE,
-          !entityTag ? delegate.entityTag : Option.NONE,
-          !expires ? delegate.expires : Option.NONE,
-          !lastModified ? delegate.lastModified : Option.NONE,
-          !location ? delegate.location : Option.NONE,
-          !proxyAuthenticationChallenges ? EMPTY_SET.addAll(delegate.proxyAuthenticationChallenges) : EMPTY_SET,
-          !retryAfter ? delegate.retryAfter : Option.NONE,
-          !server ? delegate.server : Option.NONE,
-          !setCookies ? EMPTY_SET.addAll(delegate.setCookies) : EMPTY_SET,
-          delegate.status,
-          !vary ? EMPTY_SET.addAll(delegate.vary) : EMPTY_SET,
-          !warnings ? EMPTY_SEQUENCE.addAll(delegate.warnings) :  EMPTY_SEQUENCE);
-
 abstract class Response<T> {
   factory Response(
     final Status status, {
@@ -197,7 +71,13 @@ abstract class Response<T> {
             EMPTY_SEQUENCE.addAll(firstNotNull(warnings, EMPTY_LIST)));
 
   factory Response.wrapHeaders(final Status status, final Multimap<Header, String, dynamic> headers, [final T entity = null]) =>
-       new _HeadersResponseWrapper(headers, status, new Option(entity));
+       new _ResponseImpl(
+           null, null, null, null, null, null, null, null,
+           new Option(entity),
+           null, null, null, null, null, null, null, null,
+           checkNotNull(status),
+           null, null,
+           headers);
 
   FiniteSet<RangeUnit> get acceptedRangeUnits;
 
@@ -285,150 +165,11 @@ abstract class Response<T> {
     bool warnings : false});
 }
 
-abstract class _ResponseMixin<T> implements Response<T> {
-  String toString() =>
-      _responseToString(this);
-
-  Response with_({
-    final Iterable<RangeUnit> acceptedRangeUnits,
-    final int age,
-    final Iterable<Method> allowedMethods,
-    final Iterable<ChallengeMessage> authenticationChallenges,
-    final Iterable<CacheDirective> cacheDirectives,
-    final ContentInfo contentInfo,
-    final Dictionary<Header, dynamic> customHeaders,
-    final DateTime date,
-    final entity,
-    final EntityTag entityTag,
-    final DateTime expires,
-    final DateTime lastModified,
-    final URI location,
-    final Iterable<ChallengeMessage> proxyAuthenticationChallenges,
-    final DateTime retryAfter,
-    final UserAgent server,
-    final Iterable<SetCookie> setCookies,
-    final Status status,
-    final Iterable<Header> vary,
-    final Iterable<Warning> warnings}) =>
-        _responseWith(
-            this,
-            acceptedRangeUnits,
-            age,
-            allowedMethods,
-            authenticationChallenges,
-            cacheDirectives,
-            contentInfo,
-            customHeaders,
-            date,
-            entity,
-            entityTag,
-            expires,
-            lastModified,
-            location,
-            proxyAuthenticationChallenges,
-            retryAfter,
-            server,
-            setCookies,
-            status,
-            vary,
-            warnings);
-
-  Response without({
-    final bool acceptedRangeUnits : false,
-    final bool age : false,
-    final bool allowedMethods : false,
-    final bool authenticationChallenges : false,
-    final bool cacheDirectives : false,
-    final bool contentInfo : false,
-    final bool customHeaders : false,
-    final bool date : false,
-    final bool entity : false,
-    final bool entityTag : false,
-    final bool expires : false,
-    final bool lastModified : false,
-    final bool location : false,
-    final bool proxyAuthenticationChallenges : false,
-    final bool retryAfter : false,
-    final bool server : false,
-    final bool setCookies : false,
-    final bool vary : false,
-    final bool warnings : false}) =>
-        _responseWithout(
-            this,
-            acceptedRangeUnits,
-            age,
-            allowedMethods,
-            authenticationChallenges,
-            cacheDirectives,
-            contentInfo,
-            customHeaders,
-            date,
-            entity,
-            entityTag,
-            expires,
-            lastModified,
-            location,
-            proxyAuthenticationChallenges,
-            retryAfter,
-            server,
-            setCookies,
-            vary,
-            warnings);
-}
-
 class _ResponseImpl<T>
     extends Object
-    with _ResponseMixin<T>
+    with _Parseable
     implements Response<T> {
-  final ImmutableSet<RangeUnit> acceptedRangeUnits;
-  final Option<int> age;
-  final ImmutableSet<Method> allowedMethods;
-  final ImmutableSet<ChallengeMessage> authenticationChallenges;
-  final ImmutableSet<CacheDirective> cacheDirectives;
-  final ContentInfo contentInfo;
-  final ImmutableDictionary<Header, dynamic> customHeaders;
-  final Option<DateTime> date;
   final Option<T> entity;
-  final Option<EntityTag> entityTag;
-  final Option<DateTime> expires;
-  final Option<DateTime> lastModified;
-  final Option<URI> location;
-  final ImmutableSet<ChallengeMessage> proxyAuthenticationChallenges;
-  final Option<DateTime> retryAfter;
-  final Option<UserAgent> server;
-  final FiniteSet<SetCookie> setCookies;
-  final Status status;
-  final ImmutableSet<Header> vary;
-  final ImmutableSequence<Warning> warnings;
-
-  _ResponseImpl(
-    this.acceptedRangeUnits,
-    this.age,
-    this.allowedMethods,
-    this.authenticationChallenges,
-    this.cacheDirectives,
-    this.contentInfo,
-    this.customHeaders,
-    this.date,
-    this.entity,
-    this.entityTag,
-    this.expires,
-    this.lastModified,
-    this.location,
-    this.proxyAuthenticationChallenges,
-    this.retryAfter,
-    this.server,
-    this.setCookies,
-    this.status,
-    this.vary,
-    this.warnings);
-}
-
-class _HeadersResponseWrapper<T>
-    extends Object
-    with _ResponseMixin<T>,
-      _Parseable
-    implements Response<T> {
   final Status status;
   final Multimap<Header, String, dynamic> _headers;
 
@@ -440,7 +181,6 @@ class _HeadersResponseWrapper<T>
   ContentInfo _contentInfo;
   Dictionary<Header, dynamic> _customHeaders;
   Option<DateTime> _date;
-  Option<T> entity;
   Option<EntityTag> _entityTag;
   Option<DateTime> _expires;
   Option<DateTime> _lastModified;
@@ -452,7 +192,27 @@ class _HeadersResponseWrapper<T>
   ImmutableSet<Header> _vary;
   ImmutableSequence<Warning> _warnings;
 
-  _HeadersResponseWrapper(this._headers, this.status, this.entity);
+  _ResponseImpl(
+      this._acceptedRangeUnits,
+      this._age,
+      this._allowedMethods,
+      this._authenticationChallenges,
+      this._cacheDirectives,
+      this._contentInfo,
+      this._customHeaders,
+      this._date,
+      this.entity,
+      this._entityTag,
+      this._expires,
+      this._lastModified,
+      this._location,
+      this._proxyAuthenticationChallenges,
+      this._retryAfter,
+      this._server,
+      this._setCookies,
+      this.status,
+      this._vary,
+      this._warnings, [this._headers = EMPTY_SEQUENCE_MULTIMAP]);
 
   FiniteSet<RangeUnit> get acceptedRangeUnits =>
       EMPTY_SET;
@@ -535,4 +295,130 @@ class _HeadersResponseWrapper<T>
         _warnings = _parse(_WARNING, WARNING).map(EMPTY_SEQUENCE.addAll).orElse(EMPTY_SEQUENCE);
         return _warnings;
       });
+
+  String toString() {
+    final StringBuffer buffer = new StringBuffer();
+
+    buffer.write("${status}\r\n");
+
+    writeResponseHeaders(response, _writeHeaderToBuffer(buffer));
+
+    buffer.write(response.entity.map((final entity) =>
+        "\r\n\r\n${entity.toString()}\r\n").orElse(""));
+
+    return buffer.toString();
+  }
+
+  Response with_({
+      final Iterable<RangeUnit> acceptedRangeUnits,
+      final int age,
+      final Iterable<Method> allowedMethods,
+      final Iterable<ChallengeMessage> authenticationChallenges,
+      final Iterable<CacheDirective> cacheDirectives,
+      final ContentInfo contentInfo,
+      final Dictionary<Header, dynamic> customHeaders,
+      final DateTime date,
+      final entity,
+      final EntityTag entityTag,
+      final DateTime expires,
+      final DateTime lastModified,
+      final URI location,
+      final Iterable<ChallengeMessage> proxyAuthenticationChallenges,
+      final DateTime retryAfter,
+      final UserAgent server,
+      final Iterable<SetCookie> setCookies,
+      final Status status,
+      final Iterable<Header> vary,
+      final Iterable<Warning> warnings}) {
+
+    if(isNull(acceptedRangeUnits) &&
+        isNull(age) &&
+        isNull(allowedMethods) &&
+        isNull(authenticationChallenges) &&
+        isNull(cacheDirectives) &&
+        isNull(contentInfo) &&
+        isNull(customHeaders) &&
+        isNull(date) &&
+        isNull(entity) &&
+        isNull(entityTag) &&
+        isNull(expires) &&
+        isNull(lastModified) &&
+        isNull(location) &&
+        isNull(proxyAuthenticationChallenges) &&
+        isNull(retryAfter) &&
+        isNull(server) &&
+        isNull(setCookies) &&
+        isNull(status) &&
+        isNull(vary) &&
+        isNull(warnings)) {
+      return this;
+    }
+
+    return new _ResponseImpl(
+        isNotNull(acceptedRangeUnits) ? EMPTY_SET.addAll(acceptedRangeUnits) : _acceptedRangeUnits,
+        computeIfEmpty(new Option(age), () => _age),
+        isNotNull(allowedMethods) ? EMPTY_SET.addAll(allowedMethods) : _allowedMethods,
+        isNotNull(authenticationChallenges) ? EMPTY_SET.addAll(authenticationChallenges) : _authenticationChallenges,
+        isNotNull(cacheDirectives) ? EMPTY_SET.addAll(cacheDirectives) : _cacheDirectives,
+        isNotNull(contentInfo) ? contentInfo : _contentInfo,
+        isNotNull(customHeaders) ? EMPTY_DICTIONARY.putAll(customHeaders) : _customHeaders,
+        computeIfEmpty(new Option(date), () => _date),
+        computeIfEmpty(new Option(entity), () => this.entity),
+        computeIfEmpty(new Option(entityTag), () => _entityTag),
+        computeIfEmpty(new Option(expires), () => _expires),
+        computeIfEmpty(new Option(lastModified), () => _lastModified),
+        computeIfEmpty(new Option(location), () => _location),
+        isNotNull(proxyAuthenticationChallenges) ? EMPTY_SET.addAll(proxyAuthenticationChallenges) : _proxyAuthenticationChallenges,
+        computeIfEmpty(new Option(retryAfter), () => _retryAfter),
+        computeIfEmpty(new Option(server), () => _server),
+        isNotNull(setCookies) ? EMPTY_SET.addAll(setCookies) : _setCookies,
+        firstNotNull(status, this.status),
+        isNotNull(vary) ? EMPTY_SET.addAll(vary) : _vary,
+        isNotNull(warnings) ? EMPTY_SEQUENCE.addAll(warnings) : _warnings,
+        _headers);
+  }
+
+  Response _responseWithout(
+      final Response delegate,
+      final bool acceptedRangeUnits,
+      final bool age,
+      final bool allowedMethods,
+      final bool authenticationChallenges,
+      final bool cacheDirectives,
+      final bool contentInfo,
+      final bool customHeaders,
+      final bool date,
+      final bool entity,
+      final bool entityTag,
+      final bool expires,
+      final bool lastModified,
+      final bool location,
+      final bool proxyAuthenticationChallenges,
+      final bool retryAfter,
+      final bool server,
+      final bool setCookies,
+      final bool vary,
+      final bool warnings) =>
+        new _ResponseImpl(
+          acceptedRangeUnits ? EMPTY_SET : _acceptedRangeUnits,
+          age ? Option.NONE : _age,
+          allowedMethods ? EMPTY_SET : _allowedMethods,
+          authenticationChallenges ? EMPTY_SET : _authenticationChallenges,
+          cacheDirectives ? EMPTY_SET : _cacheDirectives,
+          contentInfo ? ContentInfo.NONE : _contentInfo,
+          customHeaders ? EMPTY_DICTIONARY : _customHeaders,
+          date ? Option.NONE : _date,
+          entity ? Option.NONE : this.entity,
+          entityTag ? Option.NONE : _entityTag,
+          expires ? Option.NONE : _expires,
+          lastModified ? Option.NONE : _lastModified,
+          location ? Option.NONE : _location,
+          proxyAuthenticationChallenges ? EMPTY_SET : _proxyAuthenticationChallenges,
+          retryAfter ? Option.NONE : _retryAfter,
+          server ? Option.NONE : _server,
+          setCookies ? EMPTY_SET : _setCookies,
+          delegate.status,
+          vary ? EMPTY_SET : _vary,
+          warnings ? EMPTY_SEQUENCE : _warnings,
+          _headers);
 }
